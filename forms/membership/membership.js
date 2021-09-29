a64dc208f1bf04617b4b2a96b77d011506dd6184
Kaylee Martynow <kayleemartynow@creighton.edu>
2021-09-29 14:45:25 -0500

    calssprogress

--- forms/membership/membership.js
+++ forms/membership/membership.js
@@ -1,6 +1,12 @@
- let userName = 
+let members = ["Mary", "Sam", "Joey", "Paul", "Mike", "Char", "Jenny", "Kennedy", "Kaden", "Kurt", "Anne"]
 
-let members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
-
-
-
+btnShowMessage.onclick = function() {
+  let userName = inptName.value
+  for (i = 0; i < members.length; i++)
+    if (members.includes(userName))
+  lblDisplayName.value = userName + ", you are already a member"
+  else {
+    members.push()
+    lblDisplayName.value = userName + "You have been added to the membership roster. "
+  }
+}
